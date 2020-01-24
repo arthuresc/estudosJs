034e855b1f29aeb75a25b69d40be3da5dffe14b5
arthuresc <arthurescalera@gmail.com>
2020-01-24 01:37:46 -0300

    Aprendido e aplicado desenho pelo CSS3 icones criados

---
+++ js/todo.js
@@ -0,0 +1,9 @@
+var todos = [
+  {text: "Comprar farinha", done: false},
+  {text: "Comprar frango", done: false},
+  {text: "Comprar mandioca", done: true},
+];
+
+function teste(){
+  alert('Funcionou');
+}
